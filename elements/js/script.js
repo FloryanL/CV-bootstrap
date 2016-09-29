edcd445fb108d9043e0c71c1beb04898b71dcab6
Floryan Lollivier <floryan.lollivier@gmail.com>
2016-09-29 23:29:27 +0200

    add script in js

--- elements/js/script.js
+++ elements/js/script.js
@@ -1,1 +1,12 @@
 // ici le script
+
+//display contenue
+function contenue_display(target) {
+    document.getElementById("About").style.display = "none";
+    document.getElementById("Experience").style.display = "none";
+    document.getElementById("Competence").style.display = "none";
+    document.getElementById("Portfolio").style.display = "none";
+    document.getElementById("Hobbies").style.display = "none";
+    document.getElementById("Contact").style.display = "none";
+    document.getElementById(target).style.display = "block";
+};
